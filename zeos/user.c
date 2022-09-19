637db62e9ce91c86da48254396a5ffe1eeb4e4e8
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-09-19 10:27:00 -0400

    Main

--- zeos/user.c
+++ zeos/user.c
@@ -1,15 +1,23 @@
 #include <libc.h>
+#include <stdio.h>
 
 char buff[24];
 
 int pid;
-
-int __attribute__ ((__section__(".text.main")))
-  main(void)
+int add(int a, int b)
 {
-    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+  return a + b;
+}
+int __attribute__((__section__(".text.main")))
+main(void)
+{
+  int a = add(1, 2);
+  /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+  /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    
-  while(1) { }
+  if (a)
+
+    while (1)
+    {
+    }
 }
