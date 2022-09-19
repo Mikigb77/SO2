2256058524ad7eb0efba09e000367c8e2f3c5ad9
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-09-19 12:08:03 -0400

    Main

--- zeos/user.c
+++ zeos/user.c
@@ -8,10 +8,13 @@ int add(int a, int b)
 {
   return a + b;
 }
+extern int addASM(int a, int b);
+// extern int addASM(int a, int b);
 int __attribute__((__section__(".text.main")))
 main(void)
 {
   int a = add(1, 2);
+  a = addASM(a, 2);
   /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
   /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
