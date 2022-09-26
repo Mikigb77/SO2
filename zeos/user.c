cb285bef039e8f3b6bb47ba54fa659d944e63374
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-09-26 14:17:00 +0200

    changes

--- zeos/user.c
+++ zeos/user.c
@@ -15,8 +15,9 @@ main(void)
 {
   int a = add(1, 2);
   int b = addASM(a, 2);
+
   /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-  /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+   /*__asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) );*/
 
   while (1)
   {
