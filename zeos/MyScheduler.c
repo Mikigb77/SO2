c1fdec09e818ece66a081a0ba3e8df4664b8fb7f
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-10-31 23:34:29 +0100

    c

--- zeos/MyScheduler.c
+++ zeos/MyScheduler.c
@@ -15,7 +15,28 @@ int needs_sched_rr(void)
 }
 void update_process_state_rr(struct task_struct *t, struct list_head *dst_queue)
 {
+    list_head *l = &t->list;
+    list_del(l);
     if (dst_queue == NULL)
         return;
-    list_head *l = &t->list;
+    list_add_tail(dst_queue, l);
+}
+
+void sched_next_rr(void)
+{
+    /*first we get the new task union to implement*/
+    list_head l = list_first(readyqueue);
+    struct task_struct *t = list_head_to_task_struct(l);
+    union task_union *tu = (union task_union *)t;
+
+    /*now we need the current task union since we need to update it's state*/
+    struct task_struct *tCurr = current();
+
+    /*add the curr in a list (it could also be blocked but it's not implemented)*/
+    update_process_state_rr(tCurr, &readyqueue);
+    /*take out the new from the list it's currently in*/
+    update_process_state_rr(t, NULL);
+
+    /*finaly make the task switch*/
+    task_switch(tu);
 }
--- zeos/include/MyScheduler.h
+++ zeos/include/MyScheduler.h
@@ -1,7 +1,33 @@
 
+#ifndef _MY_SCHEDULER_
+#define _MY_SCHEDULER_
+
 #include <sched.h>
+#include "global.h"
 
 unsigned int ticks = 0;
+/**
+ * @brief Update the relevant info for the scheduling desicions
+ */
 void update_sched_data_rr(void);
+
+/**
+ * @brief Decides if the task needs to be changed
+ *
+ * @return 1 (true) if the task has to be changed. 0 if not.
+ */
 int needs_sched_rr(void);
+
+/**
+ * @brief if the task is not running it updates the state of the task (ex: from running to ready).
+ * @param t the task to update
+ * @param dst_queue the list where the task will be moved
+ */
 void update_process_state_rr(struct task_struct *t, struct list_head *dst_queue);
+
+/**
+ * @brief Decides the next proces to be executed and invokes the context switch
+ */
+void sched_next_rr(void);
+
+#endif
