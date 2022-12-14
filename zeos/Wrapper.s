424aac60f3c1836394866828931e413b39ee3765
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-12-14 13:50:29 +0100

    c

---
+++ zeos/Wrapper.s
@@ -0,0 +1,10 @@
+# 0 "Wrapper.S"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "Wrapper.S"
+# 1 "include/asm.h" 1
+# 2 "Wrapper.S" 2
+# 1 "include/segment.h" 1
+# 3 "Wrapper.S" 2
