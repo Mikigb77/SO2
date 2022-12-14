f5eaf46738a14fd277670d3143d65b56fd4f4529
Mikigb77 <miquelgarcia.alu@gmail.com>
2022-12-14 13:46:08 +0100

    c

--- zeos/user.c
+++ zeos/user.c
@@ -7,12 +7,10 @@ int pid;
 int __attribute__((__section__(".text.main")))
 main(void)
 {
-  int a = Suma(2);
   /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
   /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
   while (1)
   {
-    ++a;
   }
 }
