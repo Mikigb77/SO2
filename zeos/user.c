2bea09d84af9f05759b57164badf1d397a7d3ac4
Mikigb77 <miquelgarcia.alu@gmail.com>
2023-01-03 15:14:31 +0100

    c

--- zeos/user.c
+++ zeos/user.c
@@ -9,6 +9,7 @@ main(void)
 {
   struct stats st;
   struct stats *stp = &st;
+  get_stats(getpid(), stp);
   print_stats(stp);
   /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
   /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
